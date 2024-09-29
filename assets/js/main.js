24ea352fb81c4fbbf6f9bd78e99e1609f8a30146
Lucas Comino <lcomino3d@gmail.com>
2024-09-28 22:58:07 -0300

    Created max time config for show message and extracted config for a external file

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ const streamerBotChatOverlay = (() => {
         "id": "123123123123",
         "events": {
           "Twitch": ["ChatMessage"],
-          "YouTube": ["ChatMessage"]
+          "YouTube": ["Message"]
         }
       };
       websocket.send(JSON.stringify(subscribeMessage))
@@ -22,8 +22,10 @@ const streamerBotChatOverlay = (() => {
   let messages = []
   let config = {
     maxMessages: 10,
-    websocketAddress: 'ws://127.0.0.1:8080'
-  }
+    websocketAddress: 'ws://127.0.0.1:8080',
+    ignoredUsers: [],
+    maxTime: 15
+  }  
 
   const platformConfig = {
     "generic" : {
@@ -55,7 +57,7 @@ const streamerBotChatOverlay = (() => {
     if(array.length >= config.maxMessages){
       array.shift()          
       _removeFirstMessage()
-    }        
+    }
   }
 
   Object.defineProperty(messages, "push", {
@@ -67,12 +69,19 @@ const streamerBotChatOverlay = (() => {
     }
   });
 
+  const _generateExpireTime = () => {
+    const date = new Date()
+    date.setSeconds(date.getSeconds()+config.maxTime)
+    return date.getTime()
+  }
+
   const _createChatItem = (user, message, platform, color) => {
     const chatItem = document.createElement('div')
     const platformConfig = _getPlatformConfig(platform.toLowerCase())
     chatItem.classList.add('chat__item')
     chatItem.classList.add(platform.toLowerCase())
     chatItem.style.transform = 'translateY(20px)';
+    chatItem.setAttribute('data-max-time', _generateExpireTime())
     const chatUser = document.createElement('div')
     chatUser.classList.add('chat__user')
     chatUser.innerHTML = `<span class="platform-logo"></span> | <span class="chat__username" style="color:${color || platformConfig.color}">${user}</span>`
@@ -84,31 +93,81 @@ const streamerBotChatOverlay = (() => {
 
     return chatItem;
   }
-
+  
   const _removeFirstMessage = () => {
     let chatItens = _chatContainer.querySelectorAll('.chat__item')
     if(chatItens.length > 0){
-      chatItens[chatItens.length - 1].remove();
+      //chatItens[chatItens.length - 1].remove();
+      _removeChatElement(chatItens[chatItens.length - 1])
     }
   }
 
-  const _load = (config) => {
+  const _removeChatElement = (chatElement) => {
+    requestAnimationFrame(() => {
+      chatElement.style.opacity = 0;
+      chatElement.style.transform = 'translateY(-20px)';                      
+    });
+    setTimeout(() => { chatElement.remove() }, 500) 
+  }
+
+  const _removeExpiredMessages = () => {
+    let chatItens = _chatContainer.querySelectorAll('.chat__item')
+    if(chatItens.length > 0){
+      Array.prototype.forEach.call(chatItens, (chatElement)=>{
+        const chatMaxExpireTime = new Date(parseInt(chatElement.getAttribute('data-max-time')))
+        const now = new Date()
+        if(chatMaxExpireTime < now){
+          _removeChatElement(chatElement)         
+        }
+      })
+    }
+  }
+
+  const _getYoutubeUserColor = (userName) => {
+    const selectedColor = config.usersColor.filter((user) => {
+      return user.userName === userName
+    })
+
+    return selectedColor.length > 0 ? selectedColor[0].color : 'black'
+    
+  }
+
+  const _getUserColor = (wsData) => {
+    const color = wsData.event.source == 'Twitch' ? 
+      wsData.data.message.color : 
+      _getYoutubeUserColor(wsData.data.user.name) 
+    return color;
+  }
+
+  const _startCheckOldMessages = () => {
+    setInterval(_removeExpiredMessages, 1000)
+  }
+
+  
+
+  const _load = (newConfig) => {
+    config = newConfig
     _chatContainer = document.querySelector('.chat')
     const websocket = _subscribeWebSocket(config.websocketAddress)
-    
+    _startCheckOldMessages()
     websocket.onmessage = (message) => {
       if(message){
         const wsData = JSON.parse(message.data);
         console.log(wsData)
         if(wsData.event){
           const data = wsData.data.message
+          const message = wsData.event.source == 'Twitch' ? wsData.data.message.message : wsData.data.message
+          const color = _getUserColor(wsData)
           console.log(data)
-          let chatMessage = _createChatItem(
-              data.displayName, 
-              data.message, 
-              wsData.event.source,
-              data.color)
-          messages.push(chatMessage)
+          const userName = wsData.event.source == 'Twitch' ? data.displayName : wsData.data.user.name 
+          if(!config.ignoredUsers.includes(userName)){
+            let chatMessage = _createChatItem(
+                userName, 
+                message, 
+                wsData.event.source,
+                color)
+            messages.push(chatMessage)
+          }
         }
       }
     }
@@ -119,7 +178,14 @@ const streamerBotChatOverlay = (() => {
   }
 })()
 
-streamerBotChatOverlay.load({
-  maxMessages: 10,
-  websocketAddress: 'ws://192.168.200.114:8080'
-})
+fetch('config.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao carregar o arquivo de configuração.');
+    }
+    return response.json();
+  })
+  .then(config => {    
+    streamerBotChatOverlay.load(config)
+  })
+  .catch(error => console.error('Erro:', error));
